10a4f1cc3d9afbd02f680b5683dcd6de84690880
sanjarbekergashev <sanjarbekk9119@gmail.com>
2024-05-30 19:03:33 +0900

    feat: E -taskni bajaish

--- train.js
+++ train.js
@@ -1,16 +1,10 @@
-// D-TASK:
+// E-TASK:
 
-// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
-// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+// Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+// MASALAN: getReverse("hello") return qilsin "olleh"
 
-function checkContent(a, b) {
-  if (a.length !== b.length) {
-    return false;
-  }
-  const test1 = a.split("").sort().join();
-  const test2 = b.split("").sort().join();
-
-  return test1 === test2;
+function getReverse(a) {
+  const test = a.split("").reverse().join("");
+  console.log(test);
 }
-
-console.log(checkContent("mitgroup", "gmtiprou"));
+getReverse("hello");
