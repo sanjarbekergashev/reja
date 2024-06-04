985d23849e53ed030f3452f7936e9e09381f74d7
sanjarbekergashev <sanjarbekk9119@gmail.com>
2024-06-04 15:17:39 +0900

    feat: G - task

--- train.js
+++ train.js
@@ -1,18 +1,11 @@
-// F-TASK:
+//  G-TASK:
 
-// Shunday findDoublers function tuzing, unga faqat bitta string argument pass bolib, agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
-// MASALAN: getReverse("hello") return true return qiladi
+// Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+// MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 
-function findDoublers(str) {
-  const test = {};
-  for (let i = 0; i < str.length; i++) {
-    const a = str[i];
-    if (test[a]) {
-      return true;
-    }
-    test[a] = 1;
-  }
-  return false;
+function getHighestIndex(arr) {
+  let max = Math.max(...arr);
+  return arr.indexOf(max);
 }
 
-console.log(findDoublers("hello"));
+console.log(getHighestIndex([10, 47, -63, 3, 63]));
